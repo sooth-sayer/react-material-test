3193e97a0d653298772f2c7c280c1918b2cc1268
Dmitry Chernyatiev <d.chernyatiev@gmail.com>
2016-03-17 11:42:42 +0300

    Add router

--- app/index.js
+++ app/index.js
@@ -5,13 +5,21 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers/app';
+
+import { Router, Route, browserHistory } from 'react-router';
+
 import App from './components/App';
+import Products from './containers/Products';
 
 const store = createStore(reducers);
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="products" component={Products} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('example')
 );
